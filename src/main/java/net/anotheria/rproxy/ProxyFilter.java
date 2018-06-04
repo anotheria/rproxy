2e3e91e673dba5cfc28c6fd5f1f671957719517b
Leon Rosenberg <leon.rosenberg@anotheria.net>
2018-06-04 11:27:00 +0200

    improvement with hosts and referer

--- src/main/java/net/anotheria/rproxy/ProxyFilter.java
+++ src/main/java/net/anotheria/rproxy/ProxyFilter.java
@@ -4,12 +4,11 @@ import net.anotheria.rproxy.getter.HttpGetter;
 import net.anotheria.rproxy.getter.HttpProxyRequest;
 import net.anotheria.rproxy.getter.HttpProxyResponse;
 import net.anotheria.rproxy.replacement.URLReplacementUtil;
-import org.apache.http.HttpEntity;
+import net.anotheria.util.StringUtils;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -17,20 +16,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Enumeration;
-import java.util.Map;
 
 public class ProxyFilter implements Filter {
 
-    private static Map<String, HttpEntity> s = null;
-
-    private ServletContext context;
-
-    //TOBE Configured
+      //TOBE Configured
     String base = "http://faq.thecasuallounge.ch";
     String me = "http://localhost:8080";
 
+    //should be configured
+    String HOST_BASE = "faq.thecasuallounge.ch";
+    String HOST_ME   = "localhost:8080";
+
     public void init(FilterConfig filterConfig) throws ServletException {
-        this.context = filterConfig.getServletContext();
     }
 
     public void destroy() {
@@ -59,10 +56,19 @@ public class ProxyFilter implements Filter {
 
 		HttpProxyRequest proxyRequest = new HttpProxyRequest(base+pathToGet);
 		Enumeration<String> headerNames = req.getHeaderNames();
-		String hName;
 		while (headerNames.hasMoreElements()){
-			hName = headerNames.nextElement();
-			proxyRequest.addHeader(hName, req.getHeader(hName));
+			String hName = headerNames.nextElement();
+			String hValue = req.getHeader(hName);
+
+			if (hName.equals("referer")){
+				hValue = StringUtils.replace(hValue, me, base );
+			}
+			if (hName.equals("host")){
+				hValue = StringUtils.replace(hValue, HOST_ME, HOST_BASE);
+			}
+
+
+			proxyRequest.addHeader(hName, hValue);
 		}
 
 		HttpProxyResponse response = HttpGetter.getUrlContent(proxyRequest);
